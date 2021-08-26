a8bb70460f2569a76bc2e9ab9af9b6e6e0a5fb22
AJIN <ajinabraham7777@gmail.com>
2021-08-26 22:49:50 +0530

    Done Login GUI design

--- LoginPage.py
+++ LoginPage.py
@@ -1,42 +1,55 @@
 from os import name
 from tkinter import *
-from functools import partial
 
 if __name__ == "__main__":
+
     # Main Frame config
     mainFrame = Tk()
-    mainFrame.config(background="#212121")
-    mainFrame.geometry("1014x650")
-    mainFrame.title("Sign In")
+    mainFrame.config(background="#FE9898")
+    mainFrame.geometry("1150x650")
+    mainFrame.title("FiT wallet")
+    mainFrame.resizable(0, 0)
 
+    textLabel1 = Label(mainFrame, text="Welcome Back ,",
+                       background="#FE9898", foreground="#FFFFFF")
+    textLabel1.config(font=('Bahnschrift SemiBold', 46))
+    textLabel1.place(x=100, y=100)
+
+    textLabel2 = Label(mainFrame, text="Sign in to continue access",
+                       background="#FE9898", foreground="#FFFFFF")
+    textLabel2.config(font=('Bahnschrift Light SemiCondensed', 26))
+    textLabel2.place(x=100, y=200)
+
+    textLabel3 = Label(mainFrame, text="___________________   www.FiTbank.com",
+                       background="#FE9898", foreground="#FFFFFF")
+    textLabel3.config(font=('Bahnschrift Light SemiCondensed', 26))
+    textLabel3.place(x=100, y=500)
+    # Frame for Labels
+    frame = Frame(mainFrame, width=600, height=650, background="#F5F5F5")
+    frame.place(x=650, y=0)
+
+    pic3 = PhotoImage(file="photos\padlock.png")
+    mpic = Label(frame, image=pic3, background="#F5F5F5").place(x=320, y=100)
     # Header Config
-    Label1 = Label(mainFrame, text="Sign In",
-                   background="#222831", foreground="#0D7377")
-    Label1.config(font=('Helvatical bold', 46))
-    Label1.place(x=380, y=30)
-
-    pic1 = PhotoImage(file="photos\\Username_pic.png")
-    upic = Label(image=pic1, background="#212121").place(x=180, y=245)
-    # usernmaeLabel config
-    UnameLabel = Label(mainFrame, text="UserName",
-                       background="#212121", foreground="#FF4C29")
-    UnameLabel.config(font=('Helvatical bold', 28))
-    UnameLabel.place(x=250, y=250)
+    Label1 = Label(frame, text="Sign In",
+                   background="#F5F5F5", foreground="#000000")
+    Label1.config(font=('Arial Rounded MT Bold', 46))
+    Label1.place(x=100, y=100)
 
     # unameField config
-    unameField = Entry(mainFrame, font=('Helvatical bold', 20), background="#334756",
-                       foreground="#F7F6F2").place(x=500, y=250, height=50, width=200)
-
-    pic2 = PhotoImage(file="photos\\Password_pic.png")
-    ppic = Label(image=pic2, background="#212121").place(x=185, y=365)
-    # passWord style config
-    passLabel = Label(mainFrame, text="Password",
-                      background="#212121", foreground="#FF4C29")
-    passLabel.config(font=('Helvatical bold', 28))
-    passLabel.place(x=250, y=360)
+    pic1 = PhotoImage(file="photos\\Username_pic.png")
+    upic = Label(frame, image=pic1, background="#F5F5F5").place(x=130, y=255)
+    unameField = Entry(frame, font=('Helvatical bold', 20), background="#D3D3D3",
+                       foreground="#BFA2DB").place(x=200, y=250, height=50, width=250)
 
     # passwordField config
-    passField = Entry(mainFrame, font=('Helvatical bold', 30), show="*", background="#334756",
-                      foreground="#F7F6F2").place(x=500, y=360, height=50, width=200)
+    pic2 = PhotoImage(file="photos\\Password_pic.png")
+    ppic = Label(frame, image=pic2, background="#F5F5F5").place(x=130, y=365)
+    passField = Entry(frame, font=('Helvatical bold', 30), show="*", background="#D3D3D3",
+                      foreground="#E5FBB8").place(x=200, y=360, height=50, width=250)
+
+    submitButton = Button(frame, text='Sumbit',
+                          background='#BFA2DB', height=5, width=20)
+    submitButton.place(x=255, y=450)
 
     mainFrame.mainloop()
