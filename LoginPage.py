87fc473541865523e09e998c9e1db24710cee9e6
AJIN <ajinabraham7777@gmail.com>
2021-08-27 12:50:38 +0530

    Login DBMS completed

--- LoginPage.py
+++ LoginPage.py
@@ -1,6 +1,7 @@
 import psycopg2
-import tkinter as tk
+from tkinter import *
 from tkinter import messagebox
+from tkinter import font
 
 username = ""
 password = ""
@@ -31,69 +32,86 @@ def auth():
                                     port=db_port
                                     )
             cur = conn.cursor()
-            cur.execute
-            conn.commit()
-            conn.close()
-
-        except Exception as e:
-            messagebox.showerror("Database Error", "    Connection Error   ")
+            cur.execute(
+                "Select * from login where username=%s and password=%s", (username, password))
+            res = cur.fetchone()
             loop_number = len(username) if len(
                 username) >= len(password) else len(password)
+            if res == None:
+                messagebox.showerror("Error", "Please enter valid field")
+                for i in range(0, loop_number):
+                    passField.delete(0)
+                    unameField.delete(0)
+            else:
+                messagebox.showinfo(
+                    "Success", "               Welcome               ")
+                mainFrame.destroy()
+            conn.commit()
+
+        except Exception as e:
+            messagebox.showerror("Error", "Database Error ")
             for i in range(0, loop_number):
                 passField.delete(0)
                 unameField.delete(0)
 
+        finally:
+            conn.close()
+
 
 # Main Frame config
-mainFrame = tk.Tk()
-mainFrame.config(background="#FE9898")
+mainFrame = Tk()
+mainFrame.config(background="#D8A7B1")
 mainFrame.geometry("1150x650")
 mainFrame.title("FiT wallet")
 mainFrame.resizable(0, 0)
-textLabel1 = tk.Label(mainFrame, text="Welcome Back ,",
-                      background="#FE9898", foreground="#FFFFFF")
+
+pic0 = PhotoImage(file="photos\\Tab_icon.png")
+mainFrame.iconphoto(True, pic0)
+
+textLabel1 = Label(mainFrame, text="Welcome Back ,",
+                   background="#D8A7B1", foreground="#FFFFFF")
 textLabel1.config(font=('Bahnschrift SemiBold', 46))
 textLabel1.place(x=100, y=100)
 
-textLabel2 = tk.Label(mainFrame, text="Sign in to continue access",
-                      background="#FE9898", foreground="#FFFFFF")
+textLabel2 = Label(mainFrame, text="Sign in to continue access",
+                   background="#D8A7B1", foreground="#FFFFFF")
 textLabel2.config(font=('Bahnschrift Light SemiCondensed', 26))
 textLabel2.place(x=100, y=200)
 
-textLabel3 = tk.Label(mainFrame, text="___________________   www.FiTbank.com",
-                      background="#FE9898", foreground="#FFFFFF")
+textLabel3 = Label(mainFrame, text="___________________   www.FiTbank.com",
+                   background="#D8A7B1", foreground="#FFFFFF")
 textLabel3.config(font=('Bahnschrift Light SemiCondensed', 26))
 textLabel3.place(x=100, y=500)
 
 # secondFrame for Labels
-frame = tk.Frame(mainFrame, width=600, height=650, background="#F5F5F5")
+frame = Frame(mainFrame, width=600, height=650, background="#F5F5F5")
 frame.place(x=650, y=0)
 
 # icon config
-pic1 = tk.PhotoImage(file="photos\\Username_pic.png")
-upic = tk.Label(frame, image=pic1, background="#F5F5F5").place(x=130, y=255)
-pic2 = tk.PhotoImage(file="photos\\Password_pic.png")
-ppic = tk.Label(frame, image=pic2, background="#F5F5F5").place(x=130, y=365)
-pic3 = tk.PhotoImage(file="photos\padlock.png")
-mpic = tk.Label(frame, image=pic3, background="#F5F5F5").place(x=320, y=100)
+pic1 = PhotoImage(file="photos\\Username_pic.png")
+upic = Label(frame, image=pic1, background="#F5F5F5").place(x=130, y=255)
+pic2 = PhotoImage(file="photos\\Password_pic.png")
+ppic = Label(frame, image=pic2, background="#F5F5F5").place(x=130, y=365)
+pic3 = PhotoImage(file="photos\padlock.png")
+mpic = Label(frame, image=pic3, background="#F5F5F5").place(x=320, y=100)
 
 # Header Config
-Label1 = tk.Label(frame, text="Sign In",
-                  background="#F5F5F5", foreground="#000000")
+Label1 = Label(frame, text="Sign In",
+               background="#F5F5F5", foreground="#000000")
 Label1.config(font=('Arial Rounded MT Bold', 46))
 Label1.place(x=100, y=100)
 
 # textField Config
-unameField = tk.Entry(frame, font=('Helvatical bold', 20), background="#D3D3D3",
-                      foreground="#BFA2DB")
+unameField = Entry(frame, font=('Helvatical bold', 20), background="#D3D3D3",
+                   foreground="#BFA2DB")
 unameField.place(x=200, y=250, height=50, width=250)
-passField = tk.Entry(frame, font=('Helvatical bold', 30), show="*", background="#D3D3D3",
-                     foreground="#E5FBB8")
+passField = Entry(frame, font=('Helvatical bold', 30), show="*", background="#D3D3D3",
+                  foreground="#E5FBB8")
 passField.place(x=200, y=360, height=50, width=250)
 
 
-submitButton = tk.Button(frame, text='Sumbit', font=('Bahnschrift SemiBold', 20),
-                         background='#BFA2DB', height=1, width=10, command=auth)
+submitButton = Button(frame, text='Submit', font=('Bahnschrift SemiBold', 20),
+                      background='#BFA2DB', height=1, width=10, command=auth)
 submitButton.place(x=250, y=450)
 
 mainFrame.mainloop()
